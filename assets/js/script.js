27e5a719ea0ba6a5af6a2e2c36c61db292b0ba75
MrCCampbell <ck32xlxs@students.codeinstitute.net>
2025-05-06 13:11:31 +0100

    Restricted api key.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,10 @@
 
 
-// Google maps
+// Google map
+/
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
-  const url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBFkcCW6bk1PXGNsU9y3fKkJSS_BGViKKc"
-
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-  "marker"
-  );
   const map = new google.maps.Map(document.getElementById("cambodia-map"), {
   zoom: 7,
   center: {
@@ -20,36 +16,36 @@ async function initMap() {
   
   var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   
-  var locations = [];
-
+  var locations = [
+  { lat: 10.943501, lng: 103.273978 },
+  { lat: 12.198961, lng: 106.378396 },
+  { lat: 14.342588, lng: 103.628196 },
+  ];
+  
   const markers = locations.map((position, i) => {
-    const label = labels[i % labels.length];
-    const pinGlyph = new google.maps.marker.PinElement({
-      glyph: label,
-      glyphColor: "white",
-    });
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-      position,
-      content: pinGlyph.element,
-    });
-
-    // markers can only be keyboard focusable when they have click listeners
-    // open info window when marker is clicked
-    marker.addListener("click", () => {
-      infoWindow.setContent(position.lat + ", " + position.lng);
-      infoWindow.open(map, marker);
-    });
-    return marker;
+  const label = labels[i % labels.length];
+  const pinGlyph = new google.maps.marker.PinElement({
+  glyph: label,
+  glyphColor: "white",
   });
-
-  if (typeof markerClusterer !== 'undefined') {
-    new markerClusterer.MarkerClusterer({ markers, map });
-  } else {
-    console.error("markerClusterer is not defined. Please ensure the MarkerClusterer library is loaded.");
+  const marker = new google.maps.marker.AdvancedMarkerElement({
+  position,
+  content: pinGlyph.element,
+  });
+  
+  // markers can only be keyboard focusable when they have click listeners
+  // open info window when marker is clicked
+  marker.addListener("click", () => {
+  infoWindow.setContent(position.lat + ", " + position.lng);
+  infoWindow.open(map, marker);
+  });
+  return marker;
+  });
+  
+  new markerClusterer.MarkerClusterer({ markers, map });
   }
-}
-
-initMap();
+  
+  initMap();
 
 
 /* Google translator */
